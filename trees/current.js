98f7194a537ffed86ff75f5c48cdf9b8b789b413
Jeff Fenster <fenster.js@gmail.com>
2024-11-07 12:19:15 -0500

    v1 final

--- trees/current.js
+++ trees/current.js
@@ -1,5 +1,5 @@
 let cw, ch;
-let bottom = 50;
+let bottom = 20;
 let drawControls = false;
 let img;
 let trees = [];
@@ -29,13 +29,17 @@ function setup() {
   canvas.parent(container);
 
   fallColorFills = [
-    // 'white',
-    color(25, 70, 50),  // Orange
-    color(35, 80, 60),  // Yellow
-    color(15, 60, 40),  // Brown
-    color(45, 90, 70),  // Light Yellow
-    color(5, 70, 50),   // Red
-    color(5, 70, 50)    // Red
+    'white',
+    color(25, 70, 30),  // Orange
+    color(35, 80, 40),  // Yellow
+    color(15, 60, 35),  // Brown
+    color(45, 90, 33),  // Light Yellow
+    color(5, 70, 38),   // Red
+    color(25, 70, 60),  // Orange
+    color(35, 80, 70),  // Yellow
+    color(15, 60, 60),  // Brown
+    color(45, 90, 60),  // Light Yellow
+    color(5, 70, 60),   // Red
   ];
   lightFallColorFills = [
     // 'white',
@@ -56,48 +60,54 @@ function draw() {
 
   /** General Settings */
   let center = {x:cw/2, y:ch-bottom};
-  let treeBatchHeight = random(400,500);
-  /**
-   * Leaves:
+  let forestHeight = random(200,600);
+  /** Trunks:
+   *   1. Tree Trunks are just random bezier lines
+   */
+  let numTrunks = random(8, 15);
+  let numLinesPerTrunk = random(4,8);
+  let trunkHeight = random(90,100);
+  let trunkWidth = random(35,75)
+  /** Leaves:
    *  1. Points are draw randomly across each "row"
-   *  2. Rows increment up by rowSize until they reach treeBatchHeight
+   *  2. Rows increment up by rowHeight until they reach forestHeight
    *  3. Leaves are then drawn randomly around each point, avoiding gaps in the "arcs"
    *      - The arcs are essentially openface 3/4 circles that face the center of the tree
    *      - The idea behind arcs to avoid too much clutter in the center
    */
-  let leafWidth = random(4, 5);
-  let rowSize = 30; //x points will drawn randominly in each row. rows increment up by this amount
-  let numPointsPerRow = random(15,20);
-  let numLeavesPerPoint = random(40, 60); // # of leaves around each leaf point
-   /**
-   * Trunks:
-   *  1. Tree Trunks are just random bezier lines
-   */
-  let numTrunks = random(8, 15);
-  let numLinesPerTrunk = random(4,8);
-  let trunkHeight = random(25,50);
-  let trunkWidth = random(25,50)
+  let pointsStart = height - bottom - trunkHeight/2;
+  let numPointsPerRow = random(10,15);
+  let numLeavesPerPoint = random(1000, 1200); // # of leaves around each leaf point
+  let leafWidth = random(3, 4);
+  let rowHeight = 30; //x points will drawn randominly in each row. rows increment up by this amount
+   
 
   /** Create Tree */
-  let treeBatch = new TreeBatch({treeBatchHeight, numTrunks, numLinesPerTrunk, leafWidth, numPointsPerRow, numLeavesPerPoint, rowSize, center, trunkHeight, trunkWidth})
+  let forest = new Forest({
+    forestHeight, numTrunks, numLinesPerTrunk, leafWidth, numPointsPerRow, 
+    numLeavesPerPoint, rowHeight, center, trunkHeight, trunkWidth, pointsStart
+  })
   
   /** Create Buffers */
   let circleBuffer = createGraphics(cw, ch)
   
   //Draw Ground Fill
-  fill(fallColorFills[2])
+  let groundFill = fallColorFills[3]
+  fill(groundFill)
   rect(0, height-bottom, width, height-bottom);
   
   //Draw Ground Squiggly (on top of Ground Fill & trees)
-  drawGroundLine(100, ch-bottom, cw-100)
+  drawGroundLine(100, ch-bottom, cw-100, groundFill)
   
   //Draw Trees in order
-  treeBatch.trunks.forEach(trunk => drawTrunk(trunk, trunkHeight, trunkWidth));
-  treeBatch.circles.forEach(c => {
-    drawToBuffer(circleBuffer, c)
-  });
-  drawCircleBuffer(circleBuffer)
-  treeBatch.leaves.forEach(row => row.forEach(l => drawLeaf(l)));
+  forest.trunks.forEach(trunk => drawTrunk(trunk, trunkHeight, trunkWidth));
+  // forest.circles.forEach(c => {
+  //   drawToBuffer(circleBuffer, c)
+  // });
+  // drawCircleBuffer(circleBuffer)
+  forest.leaves.forEach(row => row.forEach(l => {
+    drawLeaf(l, 0.1, random(fallColorFills))
+  }));
   
   //Draw Texture
   blendMode(MULTIPLY);
@@ -105,23 +115,29 @@ function draw() {
   blendMode(BLEND); 
 }
 
-class TreeBatch {
-  constructor({treeBatchHeight, numTrunks, numLinesPerTrunk, leafWidth, numPointsPerRow, numLeavesPerPoint, rowSize, center, trunkHeight, trunkWidth}){
-    Object.assign(this, { treeBatchHeight, numTrunks, numLinesPerTrunk, leafWidth, numPointsPerRow, numLeavesPerPoint, rowSize, center, trunkHeight, trunkWidth });
-    this.midpoint = {x: center.x ,y: center.y - treeBatchHeight/2}
+class Forest {
+  constructor({
+    forestHeight, numTrunks, numLinesPerTrunk, leafWidth, numPointsPerRow, 
+    numLeavesPerPoint, rowHeight, center, trunkHeight, trunkWidth, pointsStart
+  }){
+    Object.assign(this, {
+      forestHeight, numTrunks, numLinesPerTrunk, leafWidth, numPointsPerRow, 
+      numLeavesPerPoint, rowHeight, center, trunkHeight, trunkWidth, pointsStart 
+    });
+    this.midpoint = {x: center.x ,y: center.y - forestHeight/2}
+    this.trunks = this.generateTrunks();
+    this.points = this.generatePoints();
+    this.circles = this.generateCircles();
+    this.leaves = this.generateLeaves();
+
     if (debug){
       fill("yellow")
-      circle(this.midpoint.x,height - bottom - treeBatchHeight,20)
+      circle(this.midpoint.x,height - bottom - forestHeight,20)
       fill("pink")
       circle(this.midpoint.x,this.midpoint.y,20)
       fill("red")
       circle(this.center.x,this.center.y,20)
     }
-    // this.circleBuffer = createGraphics(cw, ch);
-    this.trunks = this.generateTrunks();
-    this.points = this.generatePoints();
-    this.circles = this.generateCircles();
-    this.leaves = this.generateLeaves();
   }
 
   generateTrunks() {
@@ -131,7 +147,7 @@ class TreeBatch {
     for (let j = 0; j < numTrunks; j++) {
       let lines = [];
       let startPoint = {
-        x: random(100, width-100),
+        x: random(width/10, width - (width/10)),
         y: height-bottom
       };
       for (let i = 0; i < numLinesPerTrunk; i++) {
@@ -156,18 +172,16 @@ class TreeBatch {
   }
 
   generatePoints(){
-    let {treeBatchHeight, numPointsPerRow, rowSize, trunkHeight, midpoint:m} = this;
+    let {forestHeight, numPointsPerRow, rowHeight, pointsStart, midpoint:m} = this;
     let points = [];
-    let min_x = 50;
-    let max_x = width-50;
+    let min_x = width/10;
+    let max_x = width - (width/10);
 
-    let start = height - bottom;
-    let h = start - treeBatchHeight;
-    for(let i = start; i > h; i-=rowSize){
-      console.log(h, i)
+    let total_h = height - bottom - forestHeight;
+    for(let i = pointsStart; i > total_h; i-=rowHeight){
       let row = [];
       let min_y = i;
-      let max_y = i - rowSize;
+      let max_y = i - rowHeight;
       for(let j=0; j < numPointsPerRow; j++){
         let x = random(min_x, max_x);
         let y = random(min_y, max_y)
@@ -182,33 +196,35 @@ class TreeBatch {
       }
 
       // Find the point with the smallest and largest x value in the row
-      let minPoint = row.reduce((min, p) => p.x < min.x ? p : min, row[0]);
-      if (minPoint) minPoint.isLeftMost = true;
-      let maxPoint = row.reduce((max, p) => p.x > max.x ? p : max, row[0]);
-      if (maxPoint) maxPoint.isRightMost = true;
+      // let minPoint = row.reduce((min, p) => p.x < min.x ? p : min, row[0]);
+      // if (minPoint) minPoint.isLeftMost = true;
+      // let maxPoint = row.reduce((max, p) => p.x > max.x ? p : max, row[0]);
+      // if (maxPoint) maxPoint.isRightMost = true;
 
       //Push array or points in points array
       points.push(row)
 
       //Increment min/max x, while making sure we dont exceed midpoint. Otherwise, you will just start an inverted triange shape and end up with an hour glass
-      min_x += min_x > width/2 ? 0 : random(-25, 50)
-      max_x += max_x < width/2 ? 0 : random(-50, 25)
+      min_x += min_x > width/2 ? 0 : random(-(width/10), (width/10)*1.5)
+      max_x += max_x < width/2 ? 0 : random(-(width/10), (width/10)*1.5)
     }
           
     return points;
   }
 
   generatePointBoundary(px, py, mx, my){
-    let min = 50;
-    let max = 50 * (py/100);
-    let radius = random(min, max); // radius grows as we get higher, so that leaves are more spread out
+    let min = width/5 - (py/10);
+    let max = width/4 - (py/10);
+    let radius = random(min, max); 
     
     // Calculate the differences in x and y and calc angle us atan2
     let dx = mx - px;
     let dy = my - py;
     let angle = atan2(dy, dx);
-    let start = angle + QUARTER_PI
-    let stop = angle - QUARTER_PI
+    
+    // This won't do anything, but if you want to create a gap that faces center you can take the values in the comments
+    let start = 0 // angle + QUARTER_PI
+    let stop = TWO_PI // angle - QUARTER_PI
     
     return {start, stop, radius};
   }
@@ -236,87 +252,46 @@ class TreeBatch {
   generateLeaves() {
     let {leafWidth, numLeavesPerPoint, points} = this;
     let leaves = [];
-    
+
     // Create leaves that surround and face each point
     points.forEach(row => {
-      let num = numLeavesPerPoint
+      let num = numLeavesPerPoint;
       row.forEach(({x:px, y:py, boundary:b}) => {
-        // For each leaf, find a spot around the point to draw it
-        console.log("startnum", num)
+        //Create Leaves and push into row
         let row = []
         for (let i = 0; i < num; i++) {
-          console.log("i", i)
-          let w = random(leafWidth-2,leafWidth+2)
-          let h = w
-          // Only draw leaves if they fall within the boundary 
-          let angle = random(b.start, b.start + 3*HALF_PI)
-          let r = random(b.radius/3, b.radius/2) 
-          let x = px + (cos(angle) * r);
-          let y = py + (sin(angle) * r) > (height-bottom) //If y is below bottom (ground), set to y to bottom with some variance to draw "fallen leaves"
-            ? height-bottom+random(0,10) 
-            : py + (sin(angle) * r); 
-          row.push({x, y, w, h, start:angle-HALF_PI, stop:angle+HALF_PI })
+          //Width and Height of leaves
+          let leaf_w = random(leafWidth-2,leafWidth+2)
+          let leaf_h = random(leaf_w,leaf_w+4)
           
+          //Angle leaf towards center of its boundary
+          let angle = random(b.start, b.stop)
+          let r = random(0, b.radius/2) 
+          
+          //Calculate polar coordinates
+          let isFallenLeaf = py + (sin(angle) * r) >= (height-bottom) //If py is below the ground, we flag it so we can create fallen leaves later
+          let x = px + (cos(angle) * r);
+          let y = isFallenLeaf //If y is below bottom (ground), set to y to bottom with some variance to draw "fallen leaves"
+            ? height-bottom+random(0,15) 
+            : py + (sin(angle) * r);
+          angle = isFallenLeaf ? PI : angle; //Angle fallen leaves horizonally
+            
+          //Push Leaf into row
+          row.push({x, y, w:leaf_w, h:leaf_h, angle, start:angle-HALF_PI, stop:angle+HALF_PI })
+          
+          //Debug - Draw leaf point in Blue
           if (debug) {
             fill("blue")
-            circle(x,y,w)
+            circle(x,y,leaf_w)
           }
         }
+        //Push row
         leaves.push(row)
-        console.log("num", num)
-        num = num > 5 ? num - 10 : 5  //Decrement numLeavesPerPoint so that upper rows have few leaves, min 5
+        //Decrease number of leaves per point until 5 leaf minimum
+        num = num > 5 ? num - 10 : 5;
       })
     })
-
     return leaves;
-  }
-
-  drawBlob(x, y, r) {
-    beginShape();
-    
-    // Number of points to create a rounder shape
-    let points = 12;
-    let angleStep = TWO_PI / points;
-    
-    // Array to store the positions of each point
-    let vertices = [];
-    
-    // Generate vertices with random radii for irregularity
-    for (let i = 0; i < points; i++) {
-      let angle = i * angleStep;
-      let radiusOffset = random(-20, 30); // Adjusts the irregularity of the blob
-      let distance = r + radiusOffset;
-      
-      // Calculate (x, y) of each point
-      let px = x + cos(angle) * distance;
-      let py = y + sin(angle) * distance;
-      
-      vertices.push({ x: px, y: py });
-    }
-    
-    // Draw convex bezier curves between points
-    for (let i = 0; i < vertices.length; i++) {
-      let current = vertices[i];
-      let next = vertices[(i + 1) % vertices.length]; // Loop back to start
-      
-      // Control points for a convex curve
-      let control1 = {
-        x: current.x + (next.x - x) * 0.2 + random(-10, 10),
-        y: current.y + (next.y - y) * 0.2 + random(-10, 10)
-      };
-      
-      let control2 = {
-        x: next.x + (current.x - x) * 0.2 + random(-10, 10),
-        y: next.y + (current.y - y) * 0.2 + random(-10, 10)
-      };
-      
-      if (i === 0) {
-        vertex(current.x, current.y);
-      }
-      bezierVertex(control1.x, control1.y, control2.x, control2.y, next.x, next.y);
-    }
-    
-    endShape(CLOSE);
   }
 
   clear() {
@@ -325,18 +300,38 @@ class TreeBatch {
   }
 }
 
-function drawLeaf({x, y, w, h, start, stop}) {
-  //Set Styles
-  stroke("black");
-  strokeWeight(1);
-  noFill();
-  if ( random([0,1,1]) ) fill(fallColorFills[random([0,1,1,2,2,3,3,4,4])])
+function drawLeaf({x, y, w, h, angle, start, stop}, p, fill_c) {
+  let typeOfLeaf = random(0,1) > p ? "full" : "outline";
+  
+  if (typeOfLeaf === "full") {
+    drawFullLeaf()
+  } else {
+    drawOutline()
+  }
   
   // Draw the Leaf
-  arc(x, y, w, h, start, stop);
+  function drawFullLeaf() {
+    push();
+    noStroke();
+    fill(fill_c)
+    translate(x,y);
+    rotate(angle);
+    arc(0, 0, h, w, 0, TWO_PI);
+    pop();
+  }
+
+  function drawOutline() {
+    push();
+    stroke("black");
+    strokeWeight(1);
+    noFill();
+    translate(x,y);
+    arc(0, 0, w, w, start-0.2, stop+0.2); //open faced arc pointing toward boundary center
+    pop();
+  }
 }
 
-function drawToBuffer(circleBuffer, {x,y,r,isRightMost, isLeftMost}) {
+function drawToBuffer(circleBuffer, {x, y, r}) {
 
   circleBuffer.noStroke();
   circleBuffer.fill(lightFallColorFills[random([3])]);
@@ -362,8 +357,8 @@ function drawTrunk(tree, trunkHeight, trunkWidth){
     let {startPoint, controlPoints, endPoint} = line
 
     //Set Styles
-    trunkBuffer.stroke(10, 39, 14)
-    trunkBuffer.strokeWeight(1);
+    trunkBuffer.stroke(145, 77, 5)
+    trunkBuffer.strokeWeight(1.5);
     trunkBuffer.noFill()
 
     //Style the line
@@ -384,10 +379,14 @@ function drawTrunk(tree, trunkHeight, trunkWidth){
     trunkBuffer.noFill();
 
     function randomErase(){
-      trunkBuffer.fill("red")
+      if (!debug) trunkBuffer.fill("red")
       trunkBuffer.noStroke(10)
       if (!debug) trunkBuffer.erase();
-      trunkBuffer.circle(startPoint.x+random(-trunkWidth/2, trunkWidth/2), endPoint.y-trunkHeight/2, 30);
+      trunkBuffer.circle(
+        startPoint.x+random(-trunkWidth/2, trunkWidth/2), 
+        endPoint.y-trunkHeight/2, 
+        50
+      );
       if (!debug) trunkBuffer.noErase();
     }
   })
@@ -395,12 +394,12 @@ function drawTrunk(tree, trunkHeight, trunkWidth){
   image(trunkBuffer, 0, 0)
 }
 
-function drawGroundLine(xStart, yStart, xEnd){
+function drawGroundLine(xStart, yStart, xEnd, fill_c){
   let x = xStart;
   let y = yStart;
   stroke(5, 42, 12);
   strokeWeight(1);
-  noFill();
+  fill_c ? fill(fill_c) : noFill()
   
   while (x < xEnd){
     let tickLength;
